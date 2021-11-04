3b92208523699451bff6cf40e7faf1a198bdca6c
sivasankarchinnasamy <siva.vnit.1986@gmail.com>
2021-11-04 16:47:55 +0530

    project checkin

---
+++ src/main/java/com/dsa/learning/LogicMojo/App.java
@@ -0,0 +1,13 @@
+package com.dsa.learning.LogicMojo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
